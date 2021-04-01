d43d5cbf7858036ba9513bfb2aa80d5cdf029322
rajratnapatil9 <48688451+rajratnapatil9@users.noreply.github.com>
2021-04-01 00:20:59 -0400

    setting.py

--- video_stream/settings.py
+++ video_stream/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,13 +121,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
-print(BASE_DIR)
+MEDIA_URL = "/images/"
+#print(BASE_DIR)
 #Static file location
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, "static")
 ]
 
 #MEDIA FIle LOcations
-MEDIA_URL = "/images/"
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, "static/images")
