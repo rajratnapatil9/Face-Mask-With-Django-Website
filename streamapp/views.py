8410639a2ab6d1c44b421e087a316559e10ded06
rajratnapatil9 <48688451+rajratnapatil9@users.noreply.github.com>
2021-04-02 21:31:40 -0400

    20

--- streamapp/views.py
+++ streamapp/views.py
@@ -16,7 +16,9 @@ def gen(camera):
 				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-
+def video_feed(request):
+	return StreamingHttpResponse(gen(VideoCamera()),
+					content_type='multipart/x-mixed-replace; boundary=frame')
 
 
 def mask_feed(request):
